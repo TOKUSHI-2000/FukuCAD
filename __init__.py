3347d3705bf3dd681ec65e976892953df3ef3c9e
TOKUSHI-2000 <oz_sk_as_2002@outlook.jp>
2023-03-21 11:26:01 +0900

    Add __init__.py

---
+++ __init__.py
@@ -0,0 +1,56 @@
+import bpy
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTIBILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+bl_info = {
+    "name" : "FukuCAD",
+    "author" : "Tokushi-2000",
+    "description" : "",
+    "blender" : (3, 0, 1),
+    "version" : (0, 0, 1),
+    "location" : "3D View",
+    "warning" : "",
+    "suport" : "TESTING",
+    "category" : "Generic"
+}
+
+
+class AddSketchOperator(bpy.types.Operator):
+    bl_idname = "object.addsketch"
+    bl_label = "AddSketch"
+    bl_options = {'REGISTER', 'UNDO'}  # Enable undo for the operator.
+
+    def execute(self, context):        # execute() is called when running the operator.
+
+        # The original script
+        scene = context.scene
+        for obj in scene.objects:
+            obj.location.x += 1.0
+
+        return {'FINISHED'}            # Lets Blender know the operator finished successfully.
+
+def menu_func(self, context):
+    self.layout.operator(AddSketchOperator.bl_idname)
+
+def register():
+    bpy.utils.register_class(AddSketchOperator)
+    bpy.types.VIEW3D_MT_object.append(menu_func)  # Adds the new operator to an existing menu.
+
+def unregister():
+    bpy.utils.unregister_class(AddSketchOperator)
+
+
+# This allows you to run the script directly from Blender's Text editor
+# to test the add-on without having to install it.
+if __name__ == "__main__":
+    register()
